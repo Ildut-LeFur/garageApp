4eda2718ed1c9a480695c354d0f44af0105c6970
Ildut Le Fur <i7lefur@enib.fr>
2023-10-10 14:57:57 +0200

    EntityScanGarage

--- src/main/java/com/example/garageApp/GarageAppApplication.java
+++ src/main/java/com/example/garageApp/GarageAppApplication.java
@@ -1,11 +1,15 @@
 package com.example.garageApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
 @SpringBootApplication(scanBasePackages= {"com.example.garageApp"},exclude={DataSourceAutoConfiguration.class})
+@EntityScan("com.example.garageApp")
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class GarageAppApplication {
 
 	public static void main(String[] args) {
